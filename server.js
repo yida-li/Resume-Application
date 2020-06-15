acb177f4c9e46021a5e1ed5a958b2abc3c7073aa
Yida Li <gabrielyidali@gmail.com>
2020-06-14 22:19:27 -0400

    Database connected!

--- server.js
+++ server.js
@@ -2,7 +2,15 @@
 
 const express = require('express');
 
+
+
+const connectDB= require('./config/db');
+
+
 const app = express();
+
+
+connectDB();
 
 app.get('/', (req, res) => res.send('Hello Eden'));
 
