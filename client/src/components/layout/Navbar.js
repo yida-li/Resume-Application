53e52a5da5aca26bdda3448ad59af121f535b198
Yida Li <gabrielyidali@gmail.com>
2020-06-15 22:13:19 -0400

    made mistake

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Navbar = ({title,icon}) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
+
+
+export default Navbar
