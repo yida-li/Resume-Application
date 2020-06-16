13f80e4c85a31fc275e2e3e56629f635d1643d9a
Yida Li <gabrielyidali@gmail.com>
2020-06-16 19:32:18 -0400

    updated userinterface

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,65 +1,71 @@
-import React, { Fragment, useContext, useEffect } from 'react'
+import React, { Fragment, useContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom'
-import AuthContext from '../../context/auth/authContext'
-import ContactContext from '../../context/contact/contactContext'
+import { Link } from 'react-router-dom';
+import AuthContext from '../../context/auth/authContext';
+import ContactContext from '../../context/contact/contactContext';
 
 const Navbar = ({ title, icon }) => {
-    const authContext = useContext(AuthContext)
-    const contactContext = useContext(ContactContext)
-    const { isAuthenticated, logout, user, loadUser } = authContext
-    const { clearContacts } = contactContext
+  const authContext = useContext(AuthContext);
+  const contactContext = useContext(ContactContext);
+  const { isAuthenticated, logout, user, loadUser } = authContext;
+  const { clearContacts } = contactContext;
 
-    useEffect(() => {
-        loadUser()
-        //eslint-disable-next-line
-    }, [])
-    const onLogout = () =>{
-        logout()   
-        // When we logout and then try to login we see the previous user
-        //contacts for a sec. To avoid this we clear the contacts after
-        // logging out
-        clearContacts()
-    } 
-    
-    const authLinks = (
-        <Fragment>
-            <li>Hello { user && user.name }</li>
-            <li>
-                <a onClick = {onLogout} href= "#!">
-                    <i className= "fas fa-sign-out-alt"></i><span className= "hide-sm">Logout</span>
-                </a>
-            </li>
-        </Fragment>
-    )
+  useEffect(() => {
+    loadUser();
+    //eslint-disable-next-line
+  }, []);
+  const onLogout = () => {
+    logout();
+    // When we logout and then try to login we see the previous user
+    //contacts for a sec. To avoid this we clear the contacts after
+    // logging out
+    clearContacts();
+  };
 
-    const guestLinks = (
-        <Fragment>
-            <li><Link to ="/register">Register</Link></li>
-            <li><Link to ="/login">Login</Link></li>
-        </Fragment>
-    )
+  const authLinks = (
+    <Fragment>
+      <li>Hello {user && user.name}</li>
+      <li>
+        <a onClick={onLogout} href='#!'>
+          <i className='fas fa-sign-out-alt'></i>
+          <span className='hide-sm'>Logout</span>
+        </a>
+      </li>
+    </Fragment>
+  );
 
-    return (
-        <div className= "navbar bg-primary">
-            <h1>
-                <i className={icon} /> {title}               
-            </h1>
-            <ul>
-                { isAuthenticated ? authLinks : guestLinks }
-            </ul>
-        </div>
-    )
-}
+  const guestLinks = (
+    <Fragment>
+      <li>
+        <Link to='/register'>Register</Link>
+      </li>
+      <li>
+        <Link to='/login'>Login</Link>
+      </li>
+      <li>
+        <a href='https://www.w3schools.com/html/html_links.asp'>Job Search</a>
+      </li>
+    </Fragment>
+  );
+
+  return (
+    <div className='navbar bg-primary'>
+      <h1>
+        <i className='fa fa-superpowers' /> {title}
+      </h1>
+      <ul>{isAuthenticated ? authLinks : guestLinks}</ul>
+    </div>
+  );
+};
 
 Navbar.propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string,
-}
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+};
 
 Navbar.defaultProps = {
-    title: 'Contact Keeper',
-    icon: "fas fa-id-card-alt"
-}
+  title: 'Linda.com',
+  icon: 'fa fa-superpowers',
+};
 
-export default Navbar
+export default Navbar;
