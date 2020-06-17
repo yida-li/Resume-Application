3484a96b3cda162be3b4bb4051935faf337eb380
Yida Li <gabrielyidali@gmail.com>
2020-06-17 17:49:23 -0400

    solving a problem

--- README.md
+++
@@ -1,2 +0,0 @@
-# contact-keeper
-An application that allows a user to keep track of his/her contacts
--- client/src/components/layout/Spinner.js
+++
@@ -1,8 +0,0 @@
-import React, { Fragment } from 'react'
-import spinner from './spinner.gif'
-
-export default () => (
-    <Fragment>
-        <img src= {spinner} style= {{ width: '200px', margin: 'auto', display: 'block'}} alt= 'loading ...'/>
-    </Fragment>
-)
