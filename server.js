c18dd2e489a452f4c34f43fd96a0844e4d1d63ba
Yida Li <gabrielyidali@gmail.com>
2020-06-18 21:11:42 -0400

    Hey at least something is understood so far

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ const MoonBot = require('./config/db');
 
 MoonBot(); // connection with mongoDB database
 
+
+
+
+// initialize middleware 
+
+app.use(express.json({extended:false}));
+
+
+
+
+
+
+
+
+
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.json('Hello, Welcome to Infinity'));
