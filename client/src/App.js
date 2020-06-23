408ea753d4afe5cab375bf56b49f348ab9624ae1
Yida Li <gabrielyidali@gmail.com>
2020-06-23 15:29:03 -0400

    basic react front end created

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React, { Fragment } from 'react';
+
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import About from './components/pages/About';
+import Home from './components/pages/Home';
+
+const App = () => {
+  return (
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div className='container'>
+          <Switch>
+            <Route exact path='/about' component={About} />
+            <Route exact path='/' component={Home} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
+  );
+};
+
+export default App;
