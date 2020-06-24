5698c7fb2f909f4e5ad0b12130030b125c631589
Yida Li <gabrielyidali@gmail.com>
2020-06-24 18:35:30 -0400

    chapter 52

--- client/src/App.js
+++ client/src/App.js
@@ -6,23 +6,20 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import About from './components/pages/About';
 import Home from './components/pages/Home';
-import ContactState from './context/contact/ContactState';
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className='container'>
-            <Switch>
-              <Route exact path='/about' component={About} />
-              <Route exact path='/' component={Home} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div className='container'>
+          <Switch>
+            <Route exact path='/about' component={About} />
+            <Route exact path='/' component={Home} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
   );
 };
 
