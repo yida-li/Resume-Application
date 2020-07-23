412ebabaa6914acf1e073a5a56d02e2dad2b7363
Yida Li <gabrielyidali@gmail.com>
2020-07-22 23:59:16 -0400

    contact filter

--- server.js
+++ server.js
@@ -4,16 +4,12 @@ const app = express();
 const ooo = require('./config/db');
 ooo();
 
-
 // initialize middleware    use to be bodyparser, now integerated with express so lucky me eh
-app.use(express.json({extended:false}));
-
+app.use(express.json({ extended: false }));
 
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contact', require('./routes/contacts'));
 
-
-
 const PORT = process.env.PORT || 4002;
-app.listen(PORT, () => console.log('i think backend servernpconnected ok?'));
+app.listen(PORT, () => console.log('Initialized Improbability Field'));
