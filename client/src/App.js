18754f49679f78a1a152e3d14142f932b863b630
yida-li <gabrielyidali@example.com>
2021-09-02 18:20:41 -0400

    step 63

--- client/src/App.js
+++ client/src/App.js
@@ -3,27 +3,37 @@ import React, { Fragment } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import Login from './components/auth/Login';
 import About from './components/pages/About';
+import Register from './components/auth/Register';
 import Home from './components/pages/Home';
 import ContactState from './context/contact/ContactState';
-
+import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
+import Alerts from './components/layout/Alerts';
 const App = () => {
   return (
-    <ContactState>
-    <Router>
-      <Fragment>
-        <Navbar></Navbar>
+    <AuthState>
+      <ContactState>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar></Navbar>
 
-        <div className='container'>
-          <Switch>
-            <Route exact path='/' component={Home}></Route>
-            <Route exact path='/about' component={About} />
-          </Switch>
-        </div>
-      </Fragment>
-    </Router>
-    </ContactState>
+              <div className='container'>
+                <Alerts />
+                <Switch>
+                  <Route exact path='/' component={Home}></Route>
+                  <Route exact path='/register' component={Register}></Route>
+                  <Route exact path='/about' component={About} />
+                  <Route exact path='/login' component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
+      </ContactState>
+    </AuthState>
   );
 };
 
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -5,23 +5,29 @@ import { Link } from 'react-router-dom';
 
 const Navbar = ({ title, icon }) => {
   return (
-    
-     
-    
+
+
+
     <div className='navbar bg-primary'>
-     <h1>
-       <i className={icon}>{title}</i>
-     </h1>
+      <h1>
+        <i className={icon}>{title}</i>
+      </h1>
 
 
-     <ul>
-       <li>
-         <Link to = '/'> Home</Link>
-       </li>
-       <li>
-         <Link to ='/about'> About</Link>
-       </li>
-     </ul>
+      <ul>
+        <li>
+          <Link to='/'> Home</Link>
+        </li>
+        <li>
+          <Link to='/about'> About</Link>
+        </li>
+        <li>
+          <Link to='/register'> Register</Link>
+        </li>
+        <li>
+          <Link to='/login'> Login</Link>
+        </li>
+      </ul>
     </div>
   );
 };
