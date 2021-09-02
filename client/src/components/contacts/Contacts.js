64eff0504d2f99a7f024415d63733606a0184f10
yida-li <gabrielyidali@example.com>
2021-09-02 11:10:53 -0400

    step 60

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useContext } from 'react';
 import ContactContext from '../../context/contact/contactContext';
 import ContactItem from './ContactItem';
-
+import { CSSTransition,TransitionGroup } from 'react-transition-group';
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
   const { contacts, filtered } = contactContext;
@@ -12,13 +12,18 @@ const Contacts = () => {
 
   return (
     <Fragment>
+      <TransitionGroup>
       {filtered !== null
         ? filtered.map((contact) => (
-            <ContactItem key={contact.id} contact={contact} />
+          <CSSTransition key={contact.id} timeout={500} classNames='item'> 
+            <ContactItem  contact={contact} />
+            </CSSTransition>
           ))
         : contacts.map((contact) => (
-            <ContactItem key={contact.id} contact={contact} />
+          <CSSTransition key={contact.id} timeout={500} classNames='item'> 
+            <ContactItem  contact={contact} /></CSSTransition>
           ))}
+          </TransitionGroup>
     </Fragment>
   );
 };
